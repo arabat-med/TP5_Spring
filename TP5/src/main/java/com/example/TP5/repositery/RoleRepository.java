007fcb74ff44b85b4dbbc99eded4f652b439a7e4
arabat-med <130883274+arabat-med@users.noreply.github.com>
2024-12-20 20:15:07 +0100

    Delete TP5 directory

--- TP5/src/main/java/com/example/TP5/repositery/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.TP5.repositery;
-
-import com.example.TP5.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- TP5/src/main/java/com/example/TP5/repositery/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.TP5.repositery;
-
-import com.example.TP5.entity.user;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<user, Long> {
-    user findByUsername(String username);
-}
